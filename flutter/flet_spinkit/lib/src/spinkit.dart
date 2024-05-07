44c920a4339a104abc42ed1e3607c5525022d70e
InesaFitsner <inesa@appveyor.com>
2024-05-07 09:43:52 -0700

    color property

--- flutter/flet_spinkit/lib/src/spinkit.dart
+++ flutter/flet_spinkit/lib/src/spinkit.dart
@@ -31,8 +31,8 @@ class SpinkitControl extends StatelessWidget {
   Widget build(BuildContext context) {
     //debugPrint("Spinkit build: ${control.id}");
 
-    //var color = control.attrColor("color", context);
-    var color = Colors.green;
+    var color = control.attrColor("color", context);
+    //var color = Colors.green;
 
     return SpinKitRotatingCircle(
       color: color,
