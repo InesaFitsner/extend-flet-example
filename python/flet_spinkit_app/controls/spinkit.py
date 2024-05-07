e3bc8f1c534a56fb183c7998206ba263bd5c0502
InesaFitsner <inesa@appveyor.com>
2024-05-07 10:06:55 -0700

    control class in separate file

---
+++ python/flet_spinkit_app/controls/spinkit.py
@@ -0,0 +1,31 @@
+from typing import Any, Optional
+
+from flet_core.control import Control, OptionalNumber
+from flet_core.ref import Ref
+
+
+class Spinkit(Control):
+    """
+    Spinkit Control.
+    """
+
+    def __init__(
+        self,
+        #
+        # Control
+        #
+        ref: Optional[Ref] = None,
+        opacity: OptionalNumber = None,
+        visible: Optional[bool] = None,
+        data: Any = None,
+    ):
+        Control.__init__(
+            self,
+            ref=ref,
+            opacity=opacity,
+            visible=visible,
+            data=data,
+        )
+
+    def _get_control_name(self):
+        return "spinkit"
--- python/flet_spinkit_app/main.py
+++ python/flet_spinkit_app/main.py
@@ -1,35 +1,5 @@
-from typing import Any, Optional
-
 import flet as ft
-from flet_core.control import Control, OptionalNumber
-from flet_core.ref import Ref
-
-
-class Spinkit(Control):
-    """
-    Spinkit Control.
-    """
-
-    def __init__(
-        self,
-        #
-        # Control
-        #
-        ref: Optional[Ref] = None,
-        opacity: OptionalNumber = None,
-        visible: Optional[bool] = None,
-        data: Any = None,
-    ):
-        Control.__init__(
-            self,
-            ref=ref,
-            opacity=opacity,
-            visible=visible,
-            data=data,
-        )
-
-    def _get_control_name(self):
-        return "spinkit"
+from controls.spinkit import Spinkit
 
 
 def main(page: ft.Page):
