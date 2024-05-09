789771e81a17f4af2e404a8a15c3b84e9978a8b4
InesaFitsner <inesa@appveyor.com>
2024-05-09 10:55:57 -0700

    cleanup

--- python/flet_spinkit_app/controls/spinkit.py
+++ python/flet_spinkit_app/controls/spinkit.py
@@ -1,15 +1,7 @@
-from typing import Any, Optional, Union
+from typing import Any, Optional
 
 from flet_core.constrained_control import ConstrainedControl
 from flet_core.control import OptionalNumber
-from flet_core.ref import Ref
-from flet_core.types import (
-    AnimationValue,
-    OffsetValue,
-    ResponsiveNumber,
-    RotateValue,
-    ScaleValue,
-)
 
 
 class Spinkit(ConstrainedControl):
@@ -22,68 +14,28 @@ class Spinkit(ConstrainedControl):
         #
         # Control
         #
-        ref: Optional[Ref] = None,
-        expand: Union[None, bool, int] = None,
-        expand_loose: Optional[bool] = None,
-        col: Optional[ResponsiveNumber] = None,
         opacity: OptionalNumber = None,
         tooltip: Optional[str] = None,
         visible: Optional[bool] = None,
-        disabled: Optional[bool] = None,
         data: Any = None,
-        rtl: Optional[bool] = None,
         #
         # ConstrainedControl
         #
-        width: OptionalNumber = None,
-        height: OptionalNumber = None,
         left: OptionalNumber = None,
         top: OptionalNumber = None,
         right: OptionalNumber = None,
         bottom: OptionalNumber = None,
-        rotate: RotateValue = None,
-        scale: ScaleValue = None,
-        offset: OffsetValue = None,
-        aspect_ratio: OptionalNumber = None,
-        animate_opacity: AnimationValue = None,
-        animate_size: AnimationValue = None,
-        animate_position: AnimationValue = None,
-        animate_rotation: AnimationValue = None,
-        animate_scale: AnimationValue = None,
-        animate_offset: AnimationValue = None,
-        on_animation_end=None,
-        key: Optional[str] = None,
     ):
         ConstrainedControl.__init__(
             self,
-            ref=ref,
-            expand=expand,
-            expand_loose=expand_loose,
-            col=col,
             tooltip=tooltip,
             opacity=opacity,
             visible=visible,
-            disabled=disabled,
-            rtl=rtl,
             data=data,
-            width=width,
-            height=height,
             left=left,
             top=top,
             right=right,
             bottom=bottom,
-            rotate=rotate,
-            scale=scale,
-            offset=offset,
-            aspect_ratio=aspect_ratio,
-            animate_opacity=animate_opacity,
-            animate_size=animate_size,
-            animate_position=animate_position,
-            animate_rotation=animate_rotation,
-            animate_scale=animate_scale,
-            animate_offset=animate_offset,
-            on_animation_end=on_animation_end,
-            key=key,
         )
 
     def _get_control_name(self):
